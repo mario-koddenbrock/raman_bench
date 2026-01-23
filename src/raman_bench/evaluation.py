3cd7ece1e043d05fdb9a1dd1e88b73d9b0c0057e
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-23 19:10:57 +0100

    Refactor evaluation output to aggregate metrics by task type and ensure index alignment

--- src/raman_bench/evaluation.py
+++ src/raman_bench/evaluation.py
@@ -28,22 +28,49 @@ def compute_metrics_from_predictions(config):
     model_configs = config["model_configs"]
     pbar = tqdm(total=len(benchmark) * len(model_configs))
 
+    classification_output = []
+    regression_output = []
+
     for data_train, data_test, key, task_type in benchmark:
         for model_config in model_configs:
             model_name = model_config["name"]
             pbar.set_description(f"{key} | {model_name}")
 
             filename = f"{key}_{model_name}_predictions.csv"
-            y_pred = pd.read_csv(os.path.join(predictions_dir, filename))["y_pred"]
+            y_pred = pd.read_csv(os.path.join(predictions_dir, filename), index_col=0)
 
-            metrics = compute_metrics(data_test["y_true"], y_pred, task_type=task_type)
-            metrics["dataset"] = key
-            metrics["model"] = model_name
-            metrics["task_type"] = task_type
-            metrics["n_samples"] = len(data_test["y_true"])
-            metrics_df = pd.DataFrame([metrics])
-            metrics_df.to_csv(os.path.join(metrics_dir, f"metrics_{key}_{model_name}.csv"), index=False)
+            data_test = data_test.sort_index()
+            y_pred = y_pred.sort_index()
+
+            if not np.array_equal(data_test.index, y_pred.index):
+                raise ValueError(f"Indices of true values and predictions do not match for {key} and model {model_name}.")
+
+            dataset_name, target_idx, = benchmark.split_key(key)
+            output_dict = {
+                "key": key,
+                "dataset": dataset_name,
+                "task_type": task_type,
+                "target_idx": target_idx,
+                "model": model_name,
+            }
+
+            metrics = compute_metrics(data_test["target"], y_pred["target"], task_type=task_type)
+
+            output_dict.update(metrics)
+            if task_type == TASK_TYPE.Classification:
+                classification_output.append(output_dict)
+            else:
+                regression_output.append(output_dict)
+
             pbar.update(1)
+
+    if classification_output:
+        classification_df = pd.DataFrame(classification_output)
+        classification_df.to_csv(os.path.join(metrics_dir, f"classification_metrics.csv"), index=False)
+
+    if regression_output:
+        regression_df = pd.DataFrame(regression_output)
+        regression_df.to_csv(os.path.join(metrics_dir, f"regression_metrics.csv"), index=False)
 
 
 
