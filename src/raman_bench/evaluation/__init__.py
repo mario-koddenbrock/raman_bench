e2175d9c87b44e17ecbd44f133cb9f95d7b96b8a
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-23 14:28:45 +0100

    only use Autogluon for all models

--- src/raman_bench/evaluation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Evaluation module for Raman Bench.
-Provides the benchmark runner and evaluation utilities.
-"""
-from raman_bench.evaluation.runner import BenchmarkRunner
-__all__ = [
-    "BenchmarkRunner",
-]
